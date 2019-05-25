598172ab78eef26423624a63e2f57ad105b19cdc
polisetty sumanth <sumanth472@gmail.com>
2019-05-25 19:57:47 +0530

    Cors update

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.use(function(req,res,next){
    res.header("Access-Control-Allow-Origin", "*");
    res.header("Access-Control-Allow-Headers", "x-auth, Content-Type, Authorization, x-access-token");
    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-   res.setHeader('Access-Control-Allow-Credentials', true);
+   res.header('Access-Control-Allow-Credentials', true);
    next();
 });
 
